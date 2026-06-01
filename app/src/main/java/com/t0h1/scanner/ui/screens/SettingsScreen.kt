5b2aa52705e5467546a4d0ad2f80f3a91f90fe6a
tahashahrokh8804-glitch <tahashahrokh8804@gmail.com>
2026-06-01 19:57:05 +0330

    Update SettingsScreen.kt

--- app/src/main/java/com/t0h1/scanner/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/t0h1/scanner/ui/screens/SettingsScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.unit.dp
 import com.t0h1.scanner.model.ScanSettings
 
