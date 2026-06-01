90954e77ee97882580f12d750bce3c02bf0b7b52
tahashahrokh8804-glitch <tahashahrokh8804@gmail.com>
2026-06-01 18:34:33 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,10 +39,13 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+    compilerOptions {
+        jvmTarget.set(
+            org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        )
     }
-
+}
     buildFeatures {
         compose = true
     }
